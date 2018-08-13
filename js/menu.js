1c8b4805e3485859291b8a2e3d8d8ef0bf7ee84f
AndreaL <alafertteo@gmail.com>
2018-08-13 12:15:12 -0300

    menú y cambios pantalla

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,52 @@
-function toggleMenu() 
+
+//Ver perfil y ocultar lo demás
+  function verPerfil() {
+      document.getElementById('screenLogin').style.display = 'none';
+      document.getElementById('screenMenu').style.display = 'none';
+      document.getElementById('screenQA').style.display = 'none';
+      document.getElementById('screenTarifa').style.display = 'none';
+      document.getElementById('screenSaldo').style.display = 'none';
+      document.getElementById('screenProfile').style.display = 'block';
+      document.getElementById('menuStick').style.display = 'block';
+    };
+
+//Ver menú
+function verMenu() {
+    document.getElementById('screenLogin').style.display = 'none';
+    document.getElementById('screenMenu').style.display = 'block';
+    document.getElementById('screenQA').style.display = 'none';
+    document.getElementById('screenTarifa').style.display = 'none';
+    document.getElementById('screenSaldo').style.display = 'none';
+    document.getElementById('screenProfile').style.display = 'none';
+    document.getElementById('menuStick').style.display = 'block';
+  };
+  //Ver preguntas frecuentes
+  function verPreguntas() {
+      document.getElementById('screenLogin').style.display = 'none';
+      document.getElementById('screenMenu').style.display = 'none';
+      document.getElementById('screenQA').style.display = 'block';
+      document.getElementById('screenTarifa').style.display = 'none';
+      document.getElementById('screenSaldo').style.display = 'none';
+      document.getElementById('screenProfile').style.display = 'none';
+      document.getElementById('menuStick').style.display = 'block';
+    };
+  //Ver saldo
+  function verSaldo() {
+      document.getElementById('screenLogin').style.display = 'none';
+      document.getElementById('screenMenu').style.display = 'none';
+      document.getElementById('screenQA').style.display = 'none';
+      document.getElementById('screenTarifa').style.display = 'none';
+      document.getElementById('screenSaldo').style.display = 'block';
+      document.getElementById('screenProfile').style.display = 'none';
+      document.getElementById('menuStick').style.display = 'block';
+    };
+//Ver tarifa
+    function verTarifa() {
+        document.getElementById('screenLogin').style.display = 'none';
+        document.getElementById('screenMenu').style.display = 'none';
+        document.getElementById('screenQA').style.display = 'none';
+        document.getElementById('screenTarifa').style.display = 'block';
+        document.getElementById('screenSaldo').style.display = 'none';
+        document.getElementById('screenProfile').style.display = 'none';
+        document.getElementById('menuStick').style.display = 'block';
+      };
